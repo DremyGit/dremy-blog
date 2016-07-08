2908a49604f9b7cde51a028553c0711e303bee2b
dremy <fhgsm123@gmail.com>
2016-07-08 23:13:18 +0800

    Commit the manage pages

---
+++ manager/app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name managerApp
+ * @description
+ * # managerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('managerApp', ['ui.router']);
+
+angular.module('managerApp')
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: 'views/home.html'
+    });
+    $stateProvider.state('blogList', {
+      url: '/blogs',
+      templateUrl: 'views/blog/list.html',
+      controller: 'BlogListController as vm'
+    });
+    $stateProvider.state('blogEdit', {
+      url: '/blogs/:blogId',
+      templateUrl: 'views/blog/edit.html',
+      controller: 'BlogEditController as vm'
+    });
+    $stateProvider.state('blogNew', {
+      url: '/blogs/new',
+      templateUrl: 'views/blog/edit.html',
+      controller: 'BlogNewController as vm'
+    });
+    $stateProvider.state('404', {
+      url: '/404',
+      templateUrl: '404.html'
+    });
+    $urlRouterProvider.when('', '/blogs');
+    $urlRouterProvider.otherwise('/404');
+  });
