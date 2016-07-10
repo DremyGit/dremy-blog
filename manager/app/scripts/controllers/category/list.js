fc9286a744ee6308a676c29ff82a681eeefb346f
dremy <fhgsm123@gmail.com>
2016-07-10 18:58:42 +0800

    Add create for category and tag page

--- manager/app/scripts/controllers/category/list.js
+++ manager/app/scripts/controllers/category/list.js
@@ -2,29 +2,35 @@
 angular.module('managerApp').controller('CategoryListController', function ($uibModal) {
   var vm = this;
   vm.categories = [{"_id":"57301d3189d0867808ed5028","name":"分类1","code":"cate-1","count":3},{"_id":"57301d3c89d0867808ed5029","name":"分类2","code":"cate-2","count":1},{"_id":"57301f9a89d0867808ed502d","name":"分类3","code":"cate-3","count":0}];
-  vm.edit = function (category) {
-    var modalInstance = $uibModal.open({
+  function openModal(category) {
+    return $uibModal.open({
       animation: true,
       templateUrl: 'views/category/editModal.html',
       controller: 'CategoryEditModalController',
       resolve: {
-        //items: function () {
-        //  return $scope.items;
-        //}
         category: function () {
           return angular.copy(category);
         }
       }
     });
-
+  }
+  vm.create = function () {
+    var modalInstance = openModal({});
     modalInstance.result.then(function (category) {
       console.log(category);
+    });
+  };
+  vm.edit = function (category) {
+    var modalInstance = openModal(category);
+    modalInstance.result.then(function (newCategory) {
+      console.log(newCategory);
     });
   };
 });
 
 angular.module('managerApp').controller('CategoryEditModalController', function ($scope, $uibModalInstance, category) {
   $scope.category = category;
+  $scope.isNew = !category._id;
 
   $scope.ok = function () {
     $uibModalInstance.close($scope.category);
--- manager/app/scripts/controllers/tag/list.js
+++ manager/app/scripts/controllers/tag/list.js
@@ -2,8 +2,8 @@
 angular.module('managerApp').controller('TagListController', function ($uibModal) {
   var vm = this;
   vm.tags = [{"_id":"573027c7907b850c0e333979","name":"标签1","code":"tag-1","count":2},{"_id":"573027d2907b850c0e33397a","name":"标签2","code":"tag-2","count":3},{"_id":"573027da907b850c0e33397b","name":"标签3","code":"tag-3","count":0}];
-  vm.edit = function (tag) {
-    var modalInstance = $uibModal.open({
+  function openModal(tag) {
+    return $uibModal.open({
       animation: true,
       templateUrl: 'views/tag/editModal.html',
       controller: 'TagEditModalController',
@@ -13,15 +13,24 @@ angular.module('managerApp').controller('TagListController', function ($uibModal
         }
       }
     });
-
+  }
+  vm.create = function () {
+    var modalInstance = openModal({});
     modalInstance.result.then(function (tag) {
       console.log(tag);
+    });
+  };
+  vm.edit = function (tag) {
+    var modalInstance = openModal(tag);
+    modalInstance.result.then(function (newTag) {
+      console.log(newTag);
     });
   };
 });
 
 angular.module('managerApp').controller('TagEditModalController', function ($scope, $uibModalInstance, tag) {
   $scope.tag = tag;
+  $scope.isNew = !tag._id;
 
   $scope.ok = function () {
     $uibModalInstance.close($scope.tag);
