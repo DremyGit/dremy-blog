8988f497d37356e930a9fcb069858bc27a431139
dremy <fhgsm123@gmail.com>
2016-07-16 22:19:14 +0800

    Add upload feature

--- manager/app/scripts/components/mdInput.js
+++ manager/app/scripts/components/mdInput.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('managerApp').directive('mdInput', function () {
   return {
     restrict: 'AE',
@@ -9,11 +10,89 @@ angular.module('managerApp').directive('mdInput', function () {
     require: ['model', 'field'],
     templateUrl: 'views/components/mdInput.html',
     controller: 'mdInputController'
-  }
+  };
 });
-angular.module('managerApp').controller('mdInputController', function ($scope) {
-  $scope.html = '';
+angular.module('managerApp').controller('mdInputController', function ($scope, $timeout, mdService, Configs) {
+  var uploadOption = mdService.getUploadOption($scope.field, function (up, file, info) {
+    var uploadedUrl = Configs.UPLOAD_DOMAIN + '/' + JSON.parse(info).key;
+    var str = uploadedUrl;
+    var strPos = mdService.getCaretPos('input-' + $scope.field);
+    var text = $scope.model[$scope.field] || '';
+    $scope.model[$scope.field] = text.substring(0, strPos) + str + text.substring(strPos, text.length);
+    $scope.$apply();
+    mdService.setCaretPos('input-' + $scope.field, strPos + str.length);
+  });
+  var uploader;
+  $timeout(function () {
+    console.log(uploader);
+    uploader = new window.QiniuJsSDK().uploader(uploadOption);
+    console.log(uploader);
+  }, 500);
   $scope.preview = function () {
     $scope.html = $scope.model[$scope.field];
-  }
+  };
+}).service('mdService', function (Configs) {
+
+  this.getCaretPos = function (areaId) {
+    var txtarea = document.getElementById(areaId);
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart === '0') ?
+      "ff" : (document.selection ? "ie" : false ) );
+    if (br === "ie") {
+      txtarea.focus();
+      var range = document.selection.createRange();
+      range.moveStart ('character', -txtarea.value.length);
+      strPos = range.text.length;
+    } else if (br === "ff") {
+      strPos = txtarea.selectionStart;
+    }
+    return strPos;
+
+  };
+  this.setCaretPos = function (areaId, pos) {
+    var txtarea = document.getElementById(areaId);
+    var scrollPos = txtarea.scrollTop;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart === 0) ?
+      "ff" : (document.selection ? "ie" : false ) );
+    if (br === "ie") {
+      txtarea.focus();
+      var range = document.selection.createRange();
+      range.moveStart ('character', -txtarea.value.length);
+      range.moveStart ('character', pos);
+      range.moveEnd ('character', 0);
+      range.select();
+    }
+    else if (br === "ff") {
+      txtarea.selectionStart = pos;
+      txtarea.selectionEnd = pos;
+      txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
+  };
+  this.getUploadOption = function (field, callback) {
+    return {
+      runtimes: 'html5,html4',
+      browse_button: 'btn-upload-' + field,
+      uptoken_url: Configs.API_BASE + '/uptoken',
+      get_new_uptoken: false,
+      domain: Configs.UPLOAD_DOMAIN,
+      max_retries: 3,
+      dragdrop: true,
+      drop_element: 'input-' + field,
+      chunk_size: '4mb',
+      auto_start: true,
+      unique_names: false,
+      save_key: false,
+      init: {
+        FileUploaded: callback,
+        Error: function (up, err, errTip) {
+          window.alert('上传失败\n' + err + '\n' + errTip);
+        },
+        Key: function(up, file) {
+          return Configs.UPLOAD_PREFIX + file.name + Configs.UPLOAD_SUFFIX;
+        }
+      }
+    };
+
+  };
 });
--- manager/app/scripts/constants/config.js
+++ manager/app/scripts/constants/config.js
@@ -1,4 +1,7 @@
 'use strict';
 angular.module('managerApp').constant('Configs', {
-  API_BASE: '/api'
+  API_BASE: '/api',
+  UPLOAD_DOMAIN: 'http://oae5h71lc.bkt.clouddn.com',
+  UPLOAD_PREFIX: 'images/blog/test/',
+  UPLOAD_SUFFIX: '_' + new Date().getMilliseconds()
 });
