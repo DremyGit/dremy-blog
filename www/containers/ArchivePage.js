d333fc748ff73951d03c7124d8472beba5487b93
dremy <fhgsm123@gmail.com>
2016-09-08 13:31:11 +0800

    Add archive page

--- www/containers/ArchivePage.js
+++ www/containers/ArchivePage.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
+import ListBlock from '../components/ListBlock/ListBlock';
 
-const ArchivePage = () => {
-  return (
-    <div>
-      <Helmet
-        title='作品展示 Dremy_博客'
-        meta={[
-            { "name": "description", "content": "Dremy_博客 作品展示" }
+import { connect } from 'react-redux';
+import { fetchBlogListIfNeed } from '../actions/blog';
+import { dispatchFetch } from '../helpers/fetchUtils';
+
+@connect(state => ({
+  blogEntities: state.getIn(['blog', 'entities']),
+  isBlogListFetched: state.getIn(['blog', 'isFetched']),
+}))
+export default class ArchivePage extends React.Component {
+
+  static fetches = [
+    fetchBlogListIfNeed
+  ];
+
+  componentDidMount() {
+    dispatchFetch(ArchivePage.fetches, this.props);
+  }
+
+  render() {
+    const { blogEntities, isBlogListFetched } = this.props;
+    const groupedBlogs = blogEntities.groupBy(blog => blog.get('create_at').substr(0, 4));
+    if (!isBlogListFetched) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <Helmet
+          title='博客归档 Dremy_博客'
+          meta={[
+            { "name": "description", "content": "Dremy_博客 博客归档" }
           ]}
-      />
-      <h1>Archive</h1>
-    </div>
-  )
+        />
+        {groupedBlogs.entrySeq().map(([year, blogs]) =>
+            <ListBlock key={year} blogs={blogs.toList()} title={'' + year} />
+        )}
+      </div>
+    )
+  }
 };
 export default ArchivePage;
